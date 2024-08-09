b7d10e7ec59fa93ac1051d1bceb596cf9099e1e7
Rohith19 <rohith1512a@gmail.com>
2024-08-09 20:49:26 +0530

    Added production client url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,5 +4,6 @@ const allowedOrigins = [
   "http://localhost:3500",
   "http://localhost:5173",
   "http://localhost:4173",
+  "https://main--image-backup.netlify.app",
 ];
 module.exports = allowedOrigins;
