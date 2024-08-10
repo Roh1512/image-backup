79a35da6bfb74a7760554098bbf755ab776f503a
Rohith19 <rohith1512a@gmail.com>
2024-08-10 23:39:17 +0530

    Change password added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,6 +4,6 @@ const allowedOrigins = [
   "http://localhost:3500",
   "http://localhost:5173",
   "http://localhost:4173",
-  "https://main--image-backup.netlify.app",
+  "https://image-backup.netlify.app/",
 ];
 module.exports = allowedOrigins;
