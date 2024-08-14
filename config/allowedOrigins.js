cbef631fe3faa8e04982c6c631e7b6f9ef5a0523
Rohith19 <rohith1512a@gmail.com>
2024-08-15 00:45:14 +0530

    changes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,10 @@
 const allowedOrigins = [
-  /* "http://localhost:3000",
+  "http://localhost:3000",
   "http://localhost:5174",
   "http://localhost:3500",
   "http://localhost:5173",
   "http://localhost:5173/",
-  "http://localhost:4173", */
+  "http://localhost:4173",
   "https://image-backup.netlify.app/",
   "https://image-backup.netlify.app",
   "https://main--image-backup.netlify.app/",
