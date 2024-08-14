61ffbfdaee564b57849981548eb467d195864576
Rohith19 <rohith1512a@gmail.com>
2024-08-15 01:03:01 +0530

    changes

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -13,7 +13,7 @@ const handleRefreshToken = async (req, res) => {
   const refreshToken = cookies.jwt;
   res.clearCookie("jwt", {
     httpOnly: true,
-    sameSite: "Lax",
+    sameSite: "None",
     secure: process.env.NODE_ENV === "production" ? true : false,
   });
 
@@ -99,7 +99,7 @@ const handleRefreshToken = async (req, res) => {
         // Set the new refresh token as a cookie
         res.cookie("jwt", newRefreshToken, {
           httpOnly: true,
-          sameSite: "Lax",
+          sameSite: "None",
           secure: process.env.NODE_ENV === "production",
           maxAge: 24 * 60 * 60 * 1000,
         });
