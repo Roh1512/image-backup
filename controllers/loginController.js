a3fcbb08216e27aaf320731f45f4bfb75a351da4
Rohith19 <rohith1512a@gmail.com>
2024-08-18 16:24:19 +0530

    refresh debug

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -84,8 +84,8 @@ const handleLogin = [
         }
         res.clearCookie("jwt", {
           httpOnly: true,
-          sameSite: "Lax",
-          secure: process.env.NODE_ENV === "production" ? true : false,
+          /* sameSite: "Lax",
+          secure: process.env.NODE_ENV === "production" ? true : false, */
           maxAge: 24 * 60 * 60 * 1000,
         });
       }
@@ -101,8 +101,8 @@ const handleLogin = [
       process.env.NODE_ENV === "development" && console.log(updatingUser);
       res.cookie("jwt", newRefreshToken, {
         httpOnly: true,
-        sameSite: "Lax",
-        secure: process.env.NODE_ENV === "production" ? true : false,
+        /* sameSite: "Lax",
+        secure: process.env.NODE_ENV === "production" ? true : false, */
         maxAge: 24 * 60 * 60 * 1000,
       });
       process.env.NODE_ENV === "development" && console.log(accessToken);
--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -12,8 +12,8 @@ const handleRefreshToken = async (req, res) => {
   const refreshToken = cookies.jwt;
   res.clearCookie("jwt", {
     httpOnly: true,
-    sameSite: "Lax",
-    secure: process.env.NODE_ENV === "production" ? true : false,
+    /* sameSite: "Lax",
+    secure: process.env.NODE_ENV === "production" ? true : false, */
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 
@@ -99,8 +99,8 @@ const handleRefreshToken = async (req, res) => {
         // Set the new refresh token as a cookie
         res.cookie("jwt", newRefreshToken, {
           httpOnly: true,
-          sameSite: "Lax",
-          secure: process.env.NODE_ENV === "production" ? true : false,
+          /* sameSite: "Lax",
+          secure: process.env.NODE_ENV === "production" ? true : false, */
           maxAge: 24 * 60 * 60 * 1000,
         });
         res.json({ accessToken: accessToken });
