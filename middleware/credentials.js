d077a54601504460c841953a2fad9e2416c332fb
Rohith19 <rohith1512a@gmail.com>
2024-08-20 15:54:44 +0530

    refresh token debug

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -4,7 +4,7 @@ const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   console.log("Origin:", origin); // Add logging
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Credentials", true);
     process.env.NODE_ENV === "development" &&
       console.log("Access-Control-Allow-Credentials header set."); // Add logging
   }
