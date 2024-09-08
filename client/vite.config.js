0d7fdf6ff0d7713abded4665eef1a114b357356b
Rohith19 <rohith1512a@gmail.com>
2024-09-08 23:28:42 +0530

    bug fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,15 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": {
